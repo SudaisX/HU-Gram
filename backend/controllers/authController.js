8b4f99b6e906d31f3050b66caee5da538130f72b
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-01-27 00:34:39 +0500

    login route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,41 @@
 import asyncHandler from 'express-async-handler';
+import { validationResult } from 'express-validator';
+import generateToken from '../utils/generateToken.js';
 import User from '../models/userModel.js';
 
-// @desc    Test Route
+// @desc    Get User
 // @route   GET api/auth
 // @access  Public
-const auth = asyncHandler(async (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
     const user = await User.findById(req.user._id);
     res.json(user);
 });
 
-export { auth };
+// @desc    Authenticate User & Get Token
+// @route   POST api/auth
+// @access  Public
+const loginUser = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { email, password } = req.body;
+
+    // Check if User Exists
+    const user = await User.findOne({ email: email });
+
+    if (user && (await user.matchPassword(password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id),
+        });
+    } else {
+        return res.status(400).json({ errors: [{ msg: 'Invalid Credentials' }] });
+    }
+});
+
+export { getUser, loginUser };
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
-import { auth } from '../controllers/authController.js';
+import { check } from 'express-validator';
+import { getUser, loginUser } from '../controllers/authController.js';
 import { protect } from '../middleware/authMiddleware.js';
+
+// Express Router
 const router = express.Router();
 
-router.get('/', protect, auth);
+// Checks
+const validationChecks = [
+    check('email', 'Please enter a valid email address').isEmail(),
+    check('password', 'Password is required').exists(),
+];
+
+router.route('/').get(protect, getUser).post(validationChecks, loginUser);
 
 export default router;
