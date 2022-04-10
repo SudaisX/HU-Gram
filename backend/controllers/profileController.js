b616365f3394076830321b24f11ec7b3a47bec18
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-04-10 23:47:44 +0500

    created and tested create/update profile controller and route

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler';
+import { validationResult } from 'express-validator';
 import User from '../models/userModel.js';
 import Profile from '../models/profileModel.js';
 
@@ -23,4 +24,89 @@ const getCurrProfile = asyncHandler(async (req, res) => {
     }
 });
 
-export { getCurrProfile };
+// @desc    Create/Update User Profile
+// @route   POST api/profile
+// @access  Private
+const updateProfile = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+        major,
+        minor,
+        batch,
+        website,
+        birthday,
+        interests,
+        skills,
+        bio,
+        clubs,
+        hobbies,
+        twitter,
+        facebook,
+        youtube,
+        instagram,
+        github,
+        linkedin,
+    } = req.body;
+
+    // Build Profile Object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+
+    if (major) profileFields.major = major;
+    if (minor) profileFields.minor = minor;
+    if (batch) profileFields.batch = batch;
+    if (website) profileFields.website = website;
+    if (birthday) profileFields.birthday = birthday;
+    if (bio) profileFields.bio = bio;
+
+    // Handle fields with comma seperated entries
+    if (interests) {
+        profileFields.interests = interests.split(',').map((skill) => skill.trim());
+    }
+    if (skills) {
+        profileFields.skills = skills.split(',').map((skill) => skill.trim());
+    }
+    if (clubs) {
+        profileFields.clubs = clubs.split(',').map((skill) => skill.trim());
+    }
+    if (hobbies) {
+        profileFields.hobbies = hobbies.split(',').map((skill) => skill.trim());
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if (twitter) profileFields.social.twitter = twitter;
+    if (facebook) profileFields.social.facebook = facebook;
+    if (youtube) profileFields.social.youtube = youtube;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (github) profileFields.social.github = github;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+
+    try {
+        let profile = await Profile.findOne({ user: req.user.id });
+
+        if (profile) {
+            profile = await Profile.findOneAndUpdate(
+                { user: req.user.id },
+                { $set: profileFields },
+                { new: true }
+            );
+            return res.json(profile);
+        }
+
+        // Create Profile if not found
+        profile = new Profile(profileFields);
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+export { getCurrProfile, updateProfile };
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -1,8 +1,17 @@
 import express from 'express';
+import { check } from 'express-validator';
 const router = express.Router();
 import { protect } from '../middleware/authMiddleware.js';
-import { getCurrProfile } from '../controllers/profileController.js';
 
+import { getCurrProfile, updateProfile } from '../controllers/profileController.js';
+
+// Checks when you create/update profile
+const profileChecks = [
+    check('bio', 'Bio is required').notEmpty(),
+    check('skills', 'Skills is required').notEmpty(),
+];
+
+router.route('/').post(protect, profileChecks, updateProfile);
 router.route('/me').get(protect, getCurrProfile);
 
 export default router;
