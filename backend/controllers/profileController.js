5b0623a6b7f5b7f0406f1f28ab78c1f987415576
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-04-11 03:55:06 +0500

    update experience route/controller added

--- backend/controllers/profileController.js
+++ backend/controllers/profileController.js
@@ -145,4 +145,38 @@ const getUserProfile = asyncHandler(async (req, res) => {
     }
 });
 
-export { getCurrProfile, updateProfile, getAllProfiles, getUserProfile };
+// @desc    Add/Update Experience in Profile
+// @route   PUT api/profile/experience
+// @access  Private
+const updateExperience = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { title, company, location, from, to, current, description } = req.body;
+    const newExperience = {
+        title,
+        company,
+        location,
+        from,
+        to,
+        current,
+        description,
+    };
+
+    try {
+        const profile = await Profile.findOne({ user: req.user.id });
+
+        profile.experience.unshift(newExperience);
+
+        await profile.save();
+
+        res.json(profile);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+export { getCurrProfile, updateProfile, getAllProfiles, getUserProfile, updateExperience };
--- backend/routes/profileRoutes.js
+++ backend/routes/profileRoutes.js
@@ -8,6 +8,7 @@ import {
     updateProfile,
     getAllProfiles,
     getUserProfile,
+    updateExperience,
 } from '../controllers/profileController.js';
 
 // Checks when you create/update profile
@@ -16,8 +17,17 @@ const profileChecks = [
     check('skills', 'Skills is required').notEmpty(),
 ];
 
+// Checks for adding experience
+const experienceChecks = [
+    check('title', 'Title is required').notEmpty(),
+    check('company', 'Company is required').notEmpty(),
+    check('from', 'From Date is required').notEmpty(),
+];
+
 router.route('/').get(protect, getAllProfiles).post(protect, profileChecks, updateProfile);
 router.route('/me').get(protect, getCurrProfile);
+router.route('/experience').put(protect, experienceChecks, updateExperience);
+// router.route('/education').put(protect, getUserProfile);
 router.route('/user/:userId').get(protect, getUserProfile);
 
 export default router;
