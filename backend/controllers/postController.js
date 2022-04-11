50f6d5770cdfc859364f8cd3f13c3fc272f4ae37
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-04-11 05:11:39 +0500

    createpost and get all posts controller/routes

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -1,10 +1,49 @@
 import asyncHandler from 'express-async-handler';
+import { validationResult } from 'express-validator';
+import User from '../models/userModel.js';
+import Profile from '../models/profileModel.js';
+import Post from '../models/postModel.js';
 
-// @desc    Test Route
+// @desc    Create a Post
+// @route   POST api/posts
+// @access  Private
+const createPost = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+
+        const newPost = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id,
+        });
+        if (req.body.image) newPost.image = req.body.image;
+
+        const post = await newPost.save();
+
+        res.json(post);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @desc    Get all Posts
 // @route   GET api/posts
-// @access  Public
-const testController = async (req, res) => {
-    res.json({ test: 'hello' });
-};
+// @access  Private
+const getAllPosts = asyncHandler(async (req, res) => {
+    try {
+        const posts = await Post.find().sort({ createdAt: -1 });
+        res.json(posts);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
 
-export { testController };
+export { createPost, getAllPosts };
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -1,7 +1,12 @@
 import express from 'express';
 const router = express.Router();
-import { testController } from '../controllers/postController.js';
+import { check } from 'express-validator';
+import { protect } from '../middleware/authMiddleware.js';
+import { createPost, getAllPosts } from '../controllers/postController.js';
 
-router.get('/', testController);
+// Post Checks
+const postChecks = [check('text', 'Text is required').notEmpty()];
+
+router.route('/').post(protect, postChecks, createPost).get(protect, getAllPosts);
 
 export default router;
