83014d88696be6774e4b7656f63de2eff29919d2
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-04-11 06:09:54 +0500

    delete comment controller/route added

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -160,4 +160,84 @@ const unlikePost = asyncHandler(async (req, res) => {
     }
 });
 
-export { createPost, getAllPosts, getPostById, deletePostById, likePost, unlikePost };
+// @desc    Create a Comment on a Post
+// @route   POST api/posts/:id/comment
+// @access  Private
+const createComment = asyncHandler(async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = new Post({
+            text: req.body.text,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id,
+        });
+
+        post.comments.unshift(newComment);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @desc    Delete Comment from a Post by ID
+// @route   DELETE api/posts/:id/comment/:commentId
+// @access  Private
+const deleteComment = asyncHandler(async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        // Pull out comment
+        const comment = post.comments.find((comment) => comment.id === req.params.commentId);
+        if (!comment) {
+            return res.status(404).json({ msg: 'Comment does not exist' });
+        }
+
+        // Check if User owns the comment
+        if (comment.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+            .map((comment) => comment.user.toString())
+            .indexOf(req.user.id);
+        post.comments.splice(removeIndex, 1);
+
+        await post.save();
+
+        res.json(post.comments);
+    } catch (error) {
+        console.error(error.message);
+        if (error.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+});
+
+export {
+    createPost,
+    getAllPosts,
+    getPostById,
+    deletePostById,
+    likePost,
+    unlikePost,
+    createComment,
+    deleteComment,
+};
--- backend/routes/postRoutes.js
+++ backend/routes/postRoutes.js
@@ -3,7 +3,9 @@ const router = express.Router();
 import { check } from 'express-validator';
 import { protect } from '../middleware/authMiddleware.js';
 import {
+    createComment,
     createPost,
+    deleteComment,
     deletePostById,
     getAllPosts,
     getPostById,
@@ -18,5 +20,7 @@ router.route('/').post(protect, postChecks, createPost).get(protect, getAllPosts
 router.route('/:id').get(protect, getPostById).delete(protect, deletePostById);
 router.route('/:id/like').put(protect, likePost);
 router.route('/:id/unlike').put(protect, unlikePost);
+router.route('/:id/comment').post(protect, createComment);
+router.route('/:id/comment/:commentId').delete(protect, deleteComment);
 
 export default router;
