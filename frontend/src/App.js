a5b3dfbe318997616780d54cdd7d982597a55754
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-04-13 00:51:59 +0500

    addeed footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 // import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
     return (
         <>
             <Header />
             <h1>hi</h1>
+            <Footer />
         </>
     );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Nav, Navbar, NavDropdown, Container } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
