eea34053d4d53a0170ae5087af73f4572083b5c5
Sudais <44402465+sudaisx@users.noreply.github.com>
2022-04-28 05:48:00 +0500

    commit

--- backend/config/cloudinary.js
+++ backend/config/cloudinary.js
@@ -1,10 +1,17 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { CloudinaryStorage } from 'multer-storage-cloudinary';
 
+// cloudinary.config({
+//     cloud_name: process.env.CLOUDINARY_NAME,
+//     api_secret: process.env.CLOUDINARY_SECRET,
+//     api_key: process.env.CLOUDINARY_KEY,
+//     secure: true,
+// });
+
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_KEY,
-    api_secret: process.env.CLOUDINARY_SECRET,
+    cloud_name: 'hugram',
+    api_key: '497438942254757',
+    api_secret: 'QB-SyfMwIBHy73QhEZe3Y3thkws',
     secure: true,
 });
 
